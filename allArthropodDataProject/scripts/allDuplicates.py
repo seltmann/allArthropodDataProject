449be06fadf229771d7b08947e0d9aa32c4bf599
Katja Seltmann <enicospilus@gmail.com>
2017-02-28 11:38:11 -0800

    duplicates script

---
+++ allArthropodDataProject/scripts/allDuplicates.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# view_rows.py - Fetch and display the rows from a MySQL database query
+# import the MySQLdb and sys modules
+# katja seltmann April 16, 2013 to run on arthropod data in scan symbiota database
+
+import MySQLdb
+import sys
+import /raid/scratch/seltmann/allArthropodDataProject/includes.py
+
+#add date to file export
+from datetime import date
+now = date.today()
+
+#connection information from mysql
+#main database
+#connect = MySQLdb.connect("127.0.0.1", user="", passwd="", db="" )
+
+#test database
+#connect = MySQLdb.connect("127.0.0.1", user="", passwd="", db="" )
+
+cursor = connect.cursor ()
+
+#define an outfile
+outfilename = "allDuplicates_%s.tsv" % now
+outfile = open(outfilename, 'w')
+
+
+def GeoCoordinated():
+	cursor.execute ("""SELECT catalogNumber, locality, genus, specificEpithet, COUNT(*) c FROM omoccurrences WHERE catalogNumber REGEXP '[a-z]' GROUP BY catalogNumber, locality, genus, specificEpithet HAVING c > 1 limit 10;""")
+	data = cursor.fetchall()
+	for x in data:
+		concat_string = x[1]
+		print concat_string
+		catalogNumber = str(x[0])
+
+		cursor.execute ("""select collid, institutionCode, catalogNumber, locality, family, genus, specificEpithet where catalogNumber=""" +  "'" + catalogNumber + "'")
+		data = cursor.fetchall()
+		a = "Executed: %s" % catalogNumber + "\n"
+		print a
+		outfile.write(a)
+		for x in data:
+			b = "\t ".join([str(c) for c in x]) + "\n"
+			outfile.write(b)
+		outfile.write('\n')
+				
+GeoCoordinated()	
+
+#close all connections
+cursor.close()
+connect.close()
+sys.exit()
