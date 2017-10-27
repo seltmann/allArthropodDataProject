3d92bf81df365469ce47f6e768def90cef2399c2
Katja Seltmann <enicospilus@gmail.com>
2017-10-27 15:44:42 -0700

    bee queen

---
+++ allArthropodDataProject/scripts/beeQueen.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# insectList - Fetch and display the rows from a MySQL database query
+# import the MySQLdb and sys modules
+# katja seltmann October 27, 2017 to run on scan data
+
+import MySQLdb
+import sys
+import os
+os.system("python ../../includes.py")
+
+#define an outfile
+outfilename = "specimenDataBee_%s.tsv" % now
+outfile = open(outfilename, 'w')
+
+#add date to file export
+from datetime import date
+now = date.today()
+
+def insectList():
+	cursor.execute ("""select distinct sciname from omoccurrences where family='Apidae' and sciname !='Bombus' and sciname !='Bombus suckleyi ?' and sex='Female_Queen';""")
+	data = cursor.fetchall()
+	for x in data:
+		name = x[0]
+		print name
+
+        # cursor.execute ("""select distinct year, sciname, eventDate,decimalLatitude,decimalLongitude,associatedTaxa,sex from omoccurrences where decimalLatitude !='0.0000' and sciname =""" +  "'" + name + "'")
+        # data = cursor.fetchall()
+        # a = "Executed: %s" % name + "\n"
+        # print a
+        # outfile.write(a)
+        # for x in data:
+        #     b = "\t ".join([str(c) for c in x]) + "\n"
+        #     outfile.write(b)
+        # outfile.write('\n')
+				
+insectList()	
+
+
+#close all connections
+cursor.close()
+connect.close()
+sys.exit()
+
+
