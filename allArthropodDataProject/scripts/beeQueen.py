9617cee533d689a88f1dd93ff66515007187a93d
Katja Seltmann <enicospilus@gmail.com>
2017-10-27 17:33:09 -0700

    modified

--- allArthropodDataProject/scripts/beeQueen.py
+++ allArthropodDataProject/scripts/beeQueen.py
@@ -16,7 +16,7 @@ password = config.get('mysqlDB','pass')
 database = config.get('mysqlDB','db')
 
 quotes = '"'
-connect = MySQLdb.connect("127.0.0.1", user= "'" + username + "'", passwd="'" + password + "'", db="'" + database + "'"")
+connect = MySQLdb.connect("127.0.0.1", user= "'" + username + "'", passwd="'" + password + "'", db="'" + database + "'")
 cursor = connect.cursor()
 
 #add date to file export
